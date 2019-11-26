01d299409cc0155b1157c40845b47db6697c2e8a
luís fernando <munhozfontana@gmail.com>
2019-11-26 17:50:01 -0300

    implementação de textfield e textdecoration

--- lib/main.dart
+++ lib/main.dart
@@ -28,6 +28,41 @@ class _HomeState extends State<Home> {
           )
         ],
       ),
+      backgroundColor: Colors.white,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Icon(
+            Icons.person_outline,
+            size: 120.0,
+            color: Colors.green,
+          ),
+          TextField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              labelText: "Peso em (kg)",
+              labelStyle: TextStyle(color: Colors.green),
+            ),
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.green,
+              fontSize: 25.9,
+            ),
+          ),
+          TextField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              labelText: "Altura (cm)",
+              labelStyle: TextStyle(color: Colors.green),
+            ),
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.green,
+              fontSize: 25.9,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
